7bee7798d5f18ed8c6995e892c429fc254579fdf
ArpitSingh73 <arpitsingh73073@gmail.com>
2025-02-07 17:48:21 +0530

    create user register API

--- Backend/app.js
+++ Backend/app.js
@@ -5,13 +5,20 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const userRoutes = require("./routes/user.routes");
+
+
 const connectToDb = require("./db")
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 connectToDb();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/users", userRoutes);
 
 module.exports = app;
