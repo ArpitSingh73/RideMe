2a3637eabf576c9f6c1dfb7577ab722991edcca6
ArpitSingh73 <arpitsingh73073@gmail.com>
2025-02-07 20:59:19 +0530

    add profile and logout option

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
+const cookieParser = require("cookie-parser");
 
 const userRoutes = require("./routes/user.routes");
 
@@ -13,6 +14,7 @@ const connectToDb = require("./db")
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 connectToDb();
 
 app.get("/", (req, res) => {
