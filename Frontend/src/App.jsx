0bc84c044c6e692aacb28834af9e4932bbd0d156
ArpitSingh73 <arpitsingh73073@gmail.com>
2025-02-08 15:55:01 +0530

    integrate captain auth

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,9 +5,12 @@ import UserLogin from "./pages/UserLogin";
 import UserSignup from "./pages/UserSignup";
 import Captainlogin from "./pages/Captainlogin";
 import CaptainSignup from "./pages/CaptainSignup";
-import Home from "./pages/Start";
+import Home from "./pages/Home";
 import UserProtectWrapper from "./pages/UserProtectWrapper";
 import UserLogout from "./pages/UserLogout";
+import CaptainHome from "./pages/CaptainHome";
+import CaptainProtectWrapper from "./pages/CaptainProtectWrapper";
+import CaptainLogout from "./pages/CaptainLogout";
 
 const App = () => {
   return (
@@ -32,6 +35,22 @@ const App = () => {
             <UserProtectWrapper>
               <UserLogout />
             </UserProtectWrapper>
+          }
+        />
+        <Route
+          path="/captain-home"
+          element={
+            <CaptainProtectWrapper>
+              <CaptainHome />
+            </CaptainProtectWrapper>
+          }
+        />
+        <Route
+          path="/captain/logout"
+          element={
+            <CaptainProtectWrapper>
+              <CaptainLogout />
+            </CaptainProtectWrapper>
           }
         />
       </Routes>
--- Frontend/src/pages/CaptainLogin.jsx
+++ Frontend/src/pages/CaptainLogin.jsx
@@ -1,25 +1,47 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import UBERDRIVER from "../assets/uber-driver.svg";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { CaptainDataContext } from "../context/CaptainContext";
 
 const Captainlogin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [captainData, setCaptainData] = useState("");
 
-  const submitHandler = (e) => {
+  const { captain, setCaptain } = React.useContext(CaptainDataContext);
+  const navigate = useNavigate();
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-    setCaptainData({
+    const captain = {
       email: email,
       password,
-    });
+    };
+
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/captains/login`,
+      captain
+    );
+
+    if (response.status === 200) {
+      const data = response.data;
+
+      setCaptain(data.captain);
+      localStorage.setItem("token", data.token);
+      navigate("/captain-home");
+    }
+
     setEmail("");
     setPassword("");
   };
   return (
     <div className="p-7 h-screen flex flex-col justify-between">
       <div>
-        <img className="w-20 mb-3" src={UBERDRIVER} alt="" />
+        <img
+          className="w-20 mb-3"
+          src="https://www.svgrepo.com/show/505031/uber-driver.svg"
+          alt=""
+        />
 
         <form
           onSubmit={(e) => {
