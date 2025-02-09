6b519f19c916f76f140834faed08a1abc867ea69
ArpitSingh73 <arpitsingh73073@gmail.com>
2025-02-09 09:58:57 +0530

    use google maps

--- Backend/app.js
+++ Backend/app.js
@@ -1,23 +1,20 @@
-const express = require("express");
-const app = express();
-
-const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
 const cookieParser = require("cookie-parser");
-
+const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 const captainRoutes = require("./routes/captain.routes");
+const mapsRoutes = require("./routes/map.routes");
 
-
-
-const connectToDb = require("./db")
+connectToDb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-connectToDb();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -25,5 +22,6 @@ app.get("/", (req, res) => {
 
 app.use("/users", userRoutes);
 app.use("/captains", captainRoutes);
+app.use("/maps", mapsRoutes);
 
 module.exports = app;
